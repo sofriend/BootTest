0238e1cc82dafc888c431b4bc3c6ad5561e75c69
jaehwan <jaehwan@sofriend.skbroadband>
2016-02-24 23:04:23 +0900

    schema ddl create.

---
+++ BootTest/schema/ddl.sql
@@ -0,0 +1,10 @@
+create table user_info (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	userid varchar(50) not null,
+	passwd varchar(200) not null,
+	name	varchar(50) not null,
+	failcnt int,
+	lastlogintime timestamp,
+	createtime timestamp,
+	passwdchangetime timestamp
+);
