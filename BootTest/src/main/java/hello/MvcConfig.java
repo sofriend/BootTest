c59394602d770c9459d7b4cc4a4c2c7f6110caaf
jaehwan <jaehwan@sofriend>
2016-03-02 22:33:41 +0900

    security config test

--- BootTest/src/main/java/hello/MvcConfig.java
+++ BootTest/src/main/java/hello/MvcConfig.java
@@ -9,6 +9,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers( ViewControllerRegistry registry) {
+        registry.addRedirectViewController("", "/home");
         registry.addViewController("/home").setViewName("home");
         //registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
--- BootTest/src/main/java/hello/SampleController.java
+++ BootTest/src/main/java/hello/SampleController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
-
+/*
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
     }
+    */
 }
